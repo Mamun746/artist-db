a092231001f7e2d10c8555dd9ae6cf20f8630fa1
Mamun <abdullahalmamun914@gmail.com>
2019-01-04 22:10:21 +0600

    Updates

---
+++ precache-manifest.a06133501861df10272d23d7961a1af2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5ad3e67908143ac1d2e",
+    "url": "/artist-db/static/js/runtime~main.e5ad3e67.js"
+  },
+  {
+    "revision": "809180b33e4baca1deec",
+    "url": "/artist-db/static/js/main.809180b3.chunk.js"
+  },
+  {
+    "revision": "bbf346231c4fae2eeb87",
+    "url": "/artist-db/static/js/1.bbf34623.chunk.js"
+  },
+  {
+    "revision": "6450359df90d105d14e2b11d93251410",
+    "url": "/artist-db/index.html"
+  }
+];
